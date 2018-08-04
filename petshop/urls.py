8571479b0a8bc7cde84a15862ae26f65ce6647ad
Travis James Smith <trajamsmith@gmail.com>
2018-08-04 17:42:08 -0500

    Fix URL bug

--- petshop/urls.py
+++ petshop/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('adoptions/', include('adoptions.urls'))
+    # path('adoptions/', include('adoptions.urls'))
 ]
